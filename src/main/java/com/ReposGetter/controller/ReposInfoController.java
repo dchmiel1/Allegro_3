fd3745eee5b5b1510dea79562e5b10e220e21cf2
dchmiel1 <daniel.chmielewiec@gmail.com>
2021-04-23 00:01:29 +0200

    repos listing added

--- src/main/java/com/ReposGetter/controller/ReposInfoController.java
+++ src/main/java/com/ReposGetter/controller/ReposInfoController.java
@@ -1,25 +1,25 @@
 package com.ReposGetter.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.ReposGetter.model.ReposInfo;
+import com.ReposGetter.service.ReposInfoService;
 
 @RestController
 public class ReposInfoController {
 	
-	private final String INFO = "Type 'http://localhost:8080/{username}' to see his repositories and amount of stars'";
+	private final String info = "Type 'http://localhost:8080/{username}' to see his repositories names and amount of stars'";
 	
 	@RequestMapping("/")
 	public String getServiceInfo() {
-		return INFO;
+		return info;
 	}
 	
 	@GetMapping("/{name}")
 	public ReposInfo getReposInfo(@PathVariable("name") String name) {
-		return new ReposInfo(0, new ArrayList<>(Arrays.asList(name, "test", "123")));
+		return ReposInfoService.getReposInfo(name);
 	}
 }
---
+++ src/main/java/com/ReposGetter/service/ReposInfoService.java
@@ -0,0 +1,73 @@
+package com.ReposGetter.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import org.json.JSONArray;
+
+import com.ReposGetter.model.ReposInfo;
+
+public class ReposInfoService {
+
+	private final static String api_addr_1 = "https://api.github.com/users/";
+	private final static String api_addr_2 = "/repos?page=";
+
+	private static String readData(Reader reader) throws IOException {
+		StringBuilder stringBuilder = new StringBuilder();
+		int read;
+		while ((read = reader.read()) != -1) {
+			stringBuilder.append((char) read);
+		}
+		return stringBuilder.toString();
+	}
+
+	public static JSONArray getJSONArray(URL url) {
+		InputStream inputStream;
+		BufferedReader bufferedReader;
+		String jsonText;
+		try {
+			inputStream = url.openStream();
+			bufferedReader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
+			jsonText = readData(bufferedReader);
+			inputStream.close();
+			return new JSONArray(jsonText);
+		} catch (IOException e) {
+			System.out.println("User doesn't exist or API rate limit exceeded");
+			return null;
+		}
+	}
+
+	public static ReposInfo getReposInfo(String username) {
+		int page = 1;
+		ArrayList<String> names = new ArrayList<String>();
+		JSONArray jsonArray = null;
+		URL url;
+		int stars = 0;
+		do {
+			try {
+				url = new URL(api_addr_1 + username + api_addr_2 + page);
+				jsonArray = getJSONArray(url);
+				if (jsonArray == null) {
+					throw new NullPointerException();
+				}
+				for (int i = 0; i < jsonArray.length(); i++) {
+					stars += jsonArray.getJSONObject(i).getInt("stargazers_count");
+					names.add(jsonArray.getJSONObject(i).getString("name"));
+				}
+				++page;
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+				return null;
+			} catch (NullPointerException e) {
+				return null;
+			}
+		} while (jsonArray.length() != 0 || page == 200);
+		return new ReposInfo(stars, names);
+	}
+}
