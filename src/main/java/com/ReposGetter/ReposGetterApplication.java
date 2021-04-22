1887c375b3e0ff9fecd658c988f527683a042bbb
dchmiel1 <daniel.chmielewiec@gmail.com>
2021-04-22 21:18:43 +0200

    Spring boot starting app

---
+++ src/main/java/com/ReposGetter/ReposGetterApplication.java
@@ -0,0 +1,13 @@
+package com.ReposGetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReposGetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReposGetterApplication.class, args);
+	}
+
+}
